65c9c15fab9996632d8bc39afc98322f2a8f7936
kprippper <1580509@gmail.com>
2026-08-20 11:45:00 +0300

    feat(engine): create calculation engine package skeleton

---
+++ apps/calculators/engine/__init__.py
@@ -0,0 +1,15 @@
+from .base import (
+    BaseCalculator,
+    CalculatorField,
+    CalculationResult,
+    MaterialItem,
+)
+from .registry import CalculatorRegistry
+
+__all__ = [
+    "BaseCalculator",
+    "CalculatorField",
+    "CalculationResult",
+    "MaterialItem",
+    "CalculatorRegistry",
+]
