a0b46a84c31ec16aac02646a9d7a5164864ba3b2
kprippper <1580509@gmail.com>
2026-09-16 09:15:00 +0300

    feat(estimates): initialize apps.estimates for persistent project estimates

---
+++ apps/estimates/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EstimatesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.estimates'
+    verbose_name = 'Кошториси та ціни'
